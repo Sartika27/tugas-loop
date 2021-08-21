740751648314bc55f63dec51cd2be8f0bc3c834c
Sartika Purnamasari <86548604+Sartika27@users.noreply.github.com>
2021-08-21 15:59:47 +0700

    Add files via upload

---
+++ lima.js
@@ -0,0 +1,5 @@
+while(true){
+   jawaban = prompt("Tanggal berapa Ulang Tahun Indonesia?");
+if(jawaban=="17") break;
+} 
+    alert("Wah,jawabanmu sudah benar!,selamat dapat bonus voucher kursus.");
